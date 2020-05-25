545b1c439639f818a0c80be48fce8e6822b850d1
qwxying <qwxying@gmail.com>
2020-05-25 17:06:56 +0800

    deploy

---
+++ precache-manifest.61d91e1c373c84d4dafada8929b87eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c1a6413841430e1e54",
+    "url": "/morney-website/css/app.19fbbbd3.css"
+  },
+  {
+    "revision": "7b3b5e0ab6fb2fe988163a3071e39532",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "74c1a6413841430e1e54",
+    "url": "/morney-website/js/app.0ea1505a.js"
+  },
+  {
+    "revision": "fa364864eff20ade58d4",
+    "url": "/morney-website/js/chunk-vendors.56718388.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
